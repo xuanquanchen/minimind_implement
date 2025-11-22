6db9b57c370648ed094b3145d8eb766466e2ee14
‘xuanquanchen’ <75066258+xuanquanchen@users.noreply.github.com>
2025-11-22 11:17:26 -0500

    rope

--- model/model.py
+++ model/model.py
@@ -1,3 +1,6 @@
+import math
+from typing import Optional
+
 from transformers import (
     PretrainedConfig,
 )
@@ -13,7 +16,7 @@ class MiniMindConfig(PretrainedConfig):
         eos_token_id: int = 2,
         hidden_act: str = "silu",
         hidden_size: int = 512,
-        intermediate_size: int = None,
+        intermediate_size: int = None, # type: ignore
         max_position_embeddings: int = 32768,
         num_attention_heads: int = 8,
         num_hidden_layers: int = 8,
@@ -78,17 +81,68 @@ import torch.nn as nn
 
 
 class RMSNorm(nn.Module):
-    
     def __init__(self, dim: int, eps: float = 1e-5):
         super().__init__()
         self.dim = dim
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(dim))
-        
+
     def _norm(self, x):
         return torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
-    
+
     def forward(self, x):
         return self.weight * self._norm(x.float()).type_as(x) * x
-        
 
+
+# define RoPE
+def precompute_freqs_cis(
+    dim: int,
+    end: int = int(32 * 1024),
+    rope_base: float = 1e6,
+    rope_scaling: Optional[dict] = None,
+):
+    freqs = 1.0 / rope_base ** torch.arange(0, dim, 2)[: dim // 2].float() / dim
+
+    # if we use scaling (YaRN or LLaMA-NTK) then we need to modify the freqs
+    if rope_scaling is not None:
+        orig_max, factor, beta_fast, beta_slow = (
+            rope_scaling.get("original_max_position_embeddings", 2048),
+            rope_scaling.get("factor", 4),
+            rope_scaling.get("beta_fast", 4),
+            rope_scaling.get("beta_slow", 1),
+        )
+
+        corr_dim = next(
+            (i for i in range(dim // 2) if 2 * math.pi / freqs[i] > orig_max), dim // 2
+        )
+
+        power = torch.arange(0, dim // 2, device=freqs.device).float() / (
+            max(dim // 2 - 1, 1)
+        )
+
+        beta = beta_slow + (beta_fast - beta_slow) * power
+
+        scale = torch.where(
+            torch.arange(dim // 2, device=freqs.device) < corr_dim,
+            (beta * factor - beta + 1) / (beta * factor),
+            1.0 / factor,
+        )
+
+        freqs = freqs * scale
+
+    t = torch.arange(end, device=freqs.device)
+    freqs = torch.outer(t, freqs).float()  # [end, dim//2]
+
+    # We need to double the size of the matrix
+    freqs_cos = torch.cat([torch.cos(freqs), torch.cos(freqs)], dim=-1)
+    freqs_sin = torch.cat([torch.sin(freqs), torch.sin(freqs)], dim=-1)
+
+    return freqs_cos, freqs_sin
+
+
+def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
+    # [a,b] -> [-b, a]
+    def rotate_half(x):
+        return torch.cat([-x[..., x.shape[-1] // 2 :], x[..., : x.shape[-1] // 2]], dim=-1)
+    
+    
